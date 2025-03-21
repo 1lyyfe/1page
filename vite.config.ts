be75ad131149479ee00a981a6c39b2930f2619fc
Haider Ashfaq <heeydurh@gmail.com>
2025-03-21 23:18:59 +0000

    try again to fix mime issue

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,20 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
   build: {
+    outDir: 'dist',
     rollupOptions: {
       output: {
         manualChunks: undefined
       }
     }
+  },
+  server: {
+    port: 3000,
+    strictPort: true
   }
 });
